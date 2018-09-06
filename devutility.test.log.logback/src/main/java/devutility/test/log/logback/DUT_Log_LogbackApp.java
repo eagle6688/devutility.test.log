2b753b8890c08406c86d579ddaf2e57029d11071
Aldwin Su <fangyansu@163.com>
2018-09-06 17:07:43 +0800

    Update

---
+++ devutility.test.log.logback/src/main/java/devutility/test/log/logback/DUT_Log_LogbackApp.java
@@ -0,0 +1,11 @@
+package devutility.test.log.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DUT_Log_LogbackApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DUT_Log_LogbackApp.class, args);
+	}
+}
